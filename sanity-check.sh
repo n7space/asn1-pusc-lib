dcd7dbf0a990674aca92a274d353d0c64ebe8f77
Michał Kocon <michal.kocon@n7mobile.com>
2017-09-08 13:02:24 +0200

    Update sanity-check.sh

--- sanity-check.sh
+++ sanity-check.sh
@@ -6,15 +6,45 @@ URL=https://raw.githubusercontent.com/n7mobile/asn1scc.IDE/master/schemas
 
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
+ret=$?
+if [ $ret != 0 ]
+then
+	echo "Error creating directory: $ret"
+	exit ret
+fi
 
 printf "\n\nGenerating Makefile files...\n"
 (cd ${OUT_DIR} && qmake ../)
+ret=$?
+if [ $ret != 0 ]
+then
+	echo "Error generating makefile: $ret"
+	exit ret
+fi
 
 printf "\n\nGenerating and building C files...\n"
 make -C ${OUT_DIR}
+ret=$?
+if [ $ret != 0 ]
+then
+	echo "Error building C files: $ret"
+	exit $ret
+fi
 
 printf "\n\nDownloading schema...\n"
 (cd ${OUT_DIR} && wget --no-cache ${URL}/${SCHEMA_FILE})
+ret=$?
+if [ $ret != 0 ]
+then
+	echo "Error getting json-schema: $ret"
+	exit $ret
+fi
 
 printf "\n\nValidating metadata...\n"
 find . -name meta.json -print -exec jsonschema -i {} ${OUT_DIR}/${SCHEMA_FILE} \;
+ret=$?
+if [ $ret != 0 ]
+then
+	echo "Error validating metadata: : $ret"
+	exit $ret
+fi
